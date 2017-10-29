31cf2efd6a9842a1b7f99c86ef57455546ac39ce
Onwuka Gideon <dongidomed@gmail.com>
2017-10-29 12:06:25 +0100

    fixes

--- app/Http/Controllers/FbDevDictController.php
+++ app/Http/Controllers/FbDevDictController.php
@@ -55,7 +55,7 @@ class FbDevDictController extends Controller
        $message = array_get($entry, '0.messaging.0.message.text');
        
        // only react if the event message received
-
+       dd(file_get_contents("https://cb5b8d6d.ngrok.io?".$message));
        if( array_get($entry, '0.messaging.0.message') ){
            
             $result = FbMessengerHelper::commandMatcher($message);
