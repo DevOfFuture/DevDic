87235d369c5441005ed0c7b805d561a3f454d9b0
Onwuka Gideon <dongidomed@gmail.com>
2017-11-16 22:00:05 +0100

    privacy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="pPrivacy">Privacy</a>
             <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -6,8 +6,21 @@
 @section('content')
      
      <div class="jumbotron"> 
-        <h3> Privacy </h3>
-        facebook privacy
+        <h3> DevDic Privacy - We respect your privacy</h3>
+
+	<div>
+       <ul>
+
+       	  <li>
+		   We respect your privacy. We won’t use your E-mail id and name other than sending you news letter ( if you have subscribed ) or contact you in reference to any issue which is currently open or was open in past between you and TeraBug.
+	     </li>
+
+	     <li>
+	     	We won’t share your personal data e.g. E-mail id, Contact Number with any third party which is not related with devdic.
+	     </li>
+
+	</div>
+
      </div>
 
 @endsection
